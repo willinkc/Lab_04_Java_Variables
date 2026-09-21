c41582d892d1e454cd059be15bb53507c5a8ee4a
kallea <willinkc@mail.uc.edu>
2026-09-21 15:08:04 -0400

    Calculates yearly maintenance cost

---
+++ src/Lab_04_02_YearlyMaintenanceCost.java
@@ -0,0 +1,13 @@
+public class Lab_04_02_YearlyMaintenanceCost {
+    public static void main(String[] args) {
+
+
+        double winter = 500.00;
+        double spring = 346.75;
+        double summer= 500.00;
+        double fall = 467.00;
+
+        double yearlyTotal = winter + spring + summer + fall;
+        System.out.println("The total yearly maintenance cost is "+yearlyTotal);
+    }
+}
