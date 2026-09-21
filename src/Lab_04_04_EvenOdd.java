83eb283d9f9536010fc94873a0799eddc410a9dc
kallea <willinkc@mail.uc.edu>
2026-09-21 15:36:11 -0400

    Calculates yearly maintenance cost

---
+++ src/Lab_04_04_EvenOdd.java
@@ -0,0 +1,12 @@
+public class Lab_04_04_EvenOdd {
+    public static void main(String[] args) {
+
+        int numToExamine = 2;
+
+        if ( numToExamine % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
